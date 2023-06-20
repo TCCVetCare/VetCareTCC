a1f9717948e0d200a787bac3c2c9129063bc04df
danxsodev <danilloxsoliveira13@gmail.com>
2023-06-20 19:10:55 -0300

    Revert commit

--- VetCareTCC/Models/CustomerModel.cs
+++ VetCareTCC/Models/CustomerModel.cs
@@ -12,11 +12,11 @@ namespace VetCareTCC.Models
         public string idCustomer { get; set; }
 
         [Display(Name = "Nome")]
-        [Required(ErrorMessage = "Informe seu nome")]
+        [Required(ErrorMessage = "Informe o nome do cliente")]
         [MaxLength(80, ErrorMessage = "O nome deve conter no maximo 255 caracteres")]
         public string nameCustomer { get; set; }
 
-        [Display(Name = "Informe seu CPF")]
+        [Display(Name = "Número do CPF")]
         [Required(ErrorMessage = "Digite o CPF do cliente")]
         [MaxLength(11, ErrorMessage = "O CPF deve conter no maximo 11 caracteres")]
         [MinLength(11, ErrorMessage = "O CPF deve conter no minimo 11 caracters")]
@@ -24,14 +24,14 @@ namespace VetCareTCC.Models
         public string cpfCustomer { get; set; }
 
         [Display(Name = "Email")]
-        [Required(ErrorMessage = "Informe seu email")]
-        [MaxLength(50, ErrorMessage = "Email deve conter no maximo 15 caracteres")]
+        [Required(ErrorMessage = "Informe o  email do cliente")]
+        [MaxLength(50, ErrorMessage = "o Email deve conter no maximo 15 caracteres")]
         //[Remote("SelectEmailDoCliente", "Cliente", ErrorMessage = "O email já está cadastrado")]
         [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\-][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Digite um Email válido")]
         public string emailCustomer { get; set; }
 
 
-        [MaxLength(8, ErrorMessage = "a senha deve conter no maximo 8 caracteres")]
+        [MaxLength(9, ErrorMessage = "a senha deve conter no maximo 8 caracteres")]
         [Display(Name = "Password")]
         public string passwordCustomer { get; set; }
 
@@ -55,8 +55,8 @@ namespace VetCareTCC.Models
         public string streetName { get; set; }
 
 
-        [Display(Name = "Número do logradouro")]
-        [Required(ErrorMessage = "Digite o número do logradouro")]
+        [Display(Name = "NÃºmero do logradouro")]
+        [Required(ErrorMessage = "Digite o nÃºmero do logradouro")]
         public string streetNumber { get; set; }
 
         [Display(Name = "Complemento")]
